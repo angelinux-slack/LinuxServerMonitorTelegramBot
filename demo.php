6e58c2143ccd8476abe4adecb5a0091999b97e30
Angelos Rosemary <horacio@consoluciones.com.mx>
2017-01-28 15:33:22 -0600

    disable web page preview

--- demo.php
+++ demo.php
@@ -29,175 +29,131 @@ endif;
 switch ($text):
 
 	case "/apacheerrorlog":
-		$msg = "[Sat Jan 28 13:37:23 2017] [error] [client 1.2.3.4] File does not exist: /home/user/public_html/favicon.ico, referer: http://www.user.com/
-[Sat Jan 28 13:37:23 2017] [error] [client 1.2.3.4] File does not exist: /home/user/public_html/404.shtml, referer: http://www.user.com/
-[Sat Jan 28 13:38:28 2017] [error] [client 1.2.3.4] File does not exist: /home/user/public_html/?";
+		file_put_contents($file, '');
+		$msg = shell_exec("tail /var/log/httpd/error_log");
 	break;
 
 	case "/eximsendmaillog":
-		$msg = "53 /home/user1
-790 /home/user/public_html/contact
-1927 /usr/local/cpanel/whostmgr/docroot
-2812 /home/user2/public_html/spamdurectory
-3738 /etc/csf";
+		file_put_contents($file, '');
+		$msg = shell_exec("/usr/bin/sudo grep cwd /var/log/exim_mainlog | grep -v /var/spool | awk -F\"cwd=\" '{print $2}' | awk '{print $1}' | sort | uniq -c | sort -n");
 	break;
 
 	case "/firewall":
-		$msg = "CSF: Adding 1.2.3.4 to csf.allow and iptables ACCEPT...
-ACCEPT  all opt -- in !lo out *  1.2.3.4  -> 0.0.0.0/0  
-ACCEPT  all opt -- in * out !lo  0.0.0.0/0  -> 1.2.3.4  
-
-CPHulk: 1.2.3.4 has been whitelisted";
+		$status = "$text,0";
+		file_put_contents($file, $status);
+		$msg = "Escribe la ip que se pondrá en lista blanca";
 	break;	
 
 	case "/free":
-		$msg = "total       used       free     shared    buffers     cached
-Mem:          7941       7038        902          0        413       3818
--/+ buffers/cache:       2807       5134
-Swap:         8197         67       8130";
+		file_put_contents($file, '');
+		$msg = shell_exec("free -m");
 	break;
 
 	case "/last":
-		$msg = "root     pts/1        1.2.3.4   Sat Jan 28 13:05   still logged in   
-root     pts/2        4.3.2.1   Fri Jan 27 13:48 - 16:36  (02:48)    
-user1 pts/1        2.4.6.8   Fri Jan 27 13:29 - 16:03  (02:33)    
-user2 pts/2        8.6.4.2   Thu Jan 26 11:49 - 18:43  (06:54)";
+		file_put_contents($file, '');
+		$msg = shell_exec("last -20");
 	break;
 
 	case "/ls":
-		$msg = "total 33M
-drwxr-xr-x  2 root   root   4.0K Dec 21 12:56 dir1
--rw-r--r--  1 root   root   2.0K Mar 23  2015 file1.txt
--rw-r--r--  1 root   root   2.0K May 15  2015 file2.txt
-drwxr-xr-x  2 root   root   4.0K Sep 13 21:22 dir2";
+		$status = "$text,0";
+		file_put_contents($file, $status);
+		$msg = "Escribe la ruta a listar";
 	break;	
 
 	case "/ping":
-		$msg = "PING google.com (1.2.3.4) 56(84) bytes of data.
-64 bytes from site.net (1.2.3.4): icmp_seq=1 ttl=53 time=22.1 ms
-64 bytes from site.net (1.2.3.4): icmp_seq=2 ttl=53 time=22.2 ms
-64 bytes from site.net (1.2.3.4): icmp_seq=3 ttl=53 time=22.2 ms
-64 bytes from site.net (1.2.3.4): icmp_seq=4 ttl=53 time=22.2 ms
-
---- google.com ping statistics ---
-4 packets transmitted, 4 received, 0% packet loss, time 3000ms
-rtt min/avg/max/mdev = 22.149/22.190/22.207/0.150 ms";
+		$status = "$text,0";
+		file_put_contents($file, $status);
+		$msg = "Escribe el dominio o IP";
 	break;		
 
 	case "/ps":
-		$msg = "user   555999 0.0  0.0   6064   608 ?        R    13:50   0:00 grep algun filtro";
+		$status = "$text,0";
+		file_put_contents($file, $status);
+		$msg = "Escribe el texto a filtrar en los procesos";
 	break;	
 
 	case "/start" : 
+		file_put_contents($file, '');
 		$msg = "Bienvenido a Linux Server Monitor";
 	break;
 
 	case "/top" : 
-		$msg = "top - 13:52:15 up 477 days,  5:37,  1 user,  load average: 1.01, 1.05, 1.01
-Tasks: 305 total,   3 running, 294 sleeping,   0 stopped,   8 zombie
-Cpu(s):  7.1%us,  1.1%sy,  0.3%ni, 87.3%id,  4.1%wa,  0.0%hi,  0.1%si,  0.0%st
-Mem:   8132104k total,  7381568k used,   750536k free,   394496k buffers
-Swap:  8393952k total,    68784k used,  8325168k free,  3802276k cached
-
-    PID USER      PR  NI  VIRT  RES  SHR S %CPU %MEM    TIME+  COMMAND          
- 557983 mailnull  25   0  394m 321m 2528 R 98.9  4.0   0:05.14 clamscan         
- 558067 root      20   0 10472  636  524 R  7.9  0.0   0:00.04 lsof             
- 558077 user1     15   0  155m 9280 5876 S  4.0  0.1   0:00.02 php              
- 528857 mailnull  15   0 71768 5644 1736 S  2.0  0.1   0:00.75 exim             
-      1 root      15   0 10376  484  452 S  0.0  0.0   2:32.15 init             
-      3 root      39  19     0    0    0 S  0.0  0.0   0:00.00 ksoftirqd/0";
+		file_put_contents($file, '');
+		$msg = shell_exec("/usr/bin/sudo top -b -n 1 | head -n 15");
 	break;
 	
 	case "/uptime":
-		$msg = "14:04:12 up 477 days,  5:49,  1 user,  load average: 1.36, 1.24, 1.11";
+		file_put_contents($file, '');
+		$msg = shell_exec("uptime");
 	break;
 
 	case "/versions":
-		$msg = "S.O.: CentOS release X.Y (Final) 2013-01-21 05:05:37
-Apache: Server version: Apache/X.Y.Z (Unix)
-PHP: X.Y.Z
-MySQL: mysql  Ver X.Y Distrib A.B.C, for Linux (x86_64) using readline Z.X";
+		file_put_contents($file, '');
+		$cen = shell_exec("cat /etc/redhat-release");
+		$des = shell_exec("ls -lct --time-style=+\"%F %T\" / | tail -1 | awk '{print $6, $7}'");
+		$apa = shell_exec("/usr/sbin/apachectl -v | grep 'Server version:'");
+		$php = phpversion();
+		$mys = shell_exec("mysql -V");
+		$msg = "S.O.: $cen $des";
+		$msg.= "Apache: $apa";
+		$msg.= "PHP: $php";
+		$msg.= "\nMySQL: $mys";
 	break;
 
 	case "/w":
-		$msg = "14:05:42 up 477 days,  5:51,  1 user,  load average: 1.18, 1.20, 1.10
-USER     TTY      FROM              LOGIN@   IDLE   JCPU   PCPU WHAT
-root     pts/1    1.2.3.4   		13:05   26:19   0.01s  0.00s nano";
+		file_put_contents($file, '');
+		$msg = shell_exec("w");
 	break;
 
 	case "/whois":
-		$msg = "[Querying whois.verisign-grs.com]
-[Redirected to whois.markmonitor.com]
-[Querying whois.markmonitor.com]
-[whois.markmonitor.com]
-Domain Name: google.com
-Registry Domain ID: 2138514_DOMAIN_COM-VRSN
-Registrar WHOIS Server: whois.markmonitor.com
-Registrar URL: http://www.markmonitor.com
-Updated Date: 2015-06-12T10:38:52-0700
-Creation Date: 1997-09-15T00:00:00-0700
-Registrar Registration Expiration Date: 2020-09-13T21:00:00-0700
-Registrar: MarkMonitor, Inc.
-Registrar IANA ID: 292
-Registrar Abuse Contact Email: abusecomplaints@markmonitor.com
-Registrar Abuse Contact Phone: +1.2083895740
-Domain Status: clientUpdateProhibited (https://www.icann.org/epp#clientUpdateProhibited)
-Domain Status: clientTransferProhibited (https://www.icann.org/epp#clientTransferProhibited)
-Domain Status: clientDeleteProhibited (https://www.icann.org/epp#clientDeleteProhibited)
-Domain Status: serverUpdateProhibited (https://www.icann.org/epp#serverUpdateProhibited)
-Domain Status: serverTransferProhibited (https://www.icann.org/epp#serverTransferProhibited)
-Domain Status: serverDeleteProhibited (https://www.icann.org/epp#serverDeleteProhibited)
-Registry Registrant ID: 
-Registrant Name: Dns Admin
-Registrant Organization: Google Inc.
-Registrant Street: Please contact contact-admin@google.com, 1600 Amphitheatre Parkway
-Registrant City: Mountain View
-Registrant State/Province: CA
-Registrant Postal Code: 94043
-Registrant Country: US
-Registrant Phone: +1.6502530000
-Registrant Phone Ext: 
-Registrant Fax: +1.6506188571
-Registrant Fax Ext: 
-Registrant Email: dns-admin@google.com
-Registry Admin ID: 
-Admin Name: DNS Admin
-Admin Organization: Google Inc.
-Admin Street: 1600 Amphitheatre Parkway
-Admin City: Mountain View
-Admin State/Province: CA
-Admin Postal Code: 94043
-Admin Country: US
-Admin Phone: +1.6506234000
-Admin Phone Ext: 
-Admin Fax: +1.6506188571
-Admin Fax Ext: 
-Admin Email: dns-admin@google.com
-Registry Tech ID: 
-Tech Name: DNS Admin
-Tech Organization: Google Inc.
-Tech Street: 2400 E. Bayshore Pkwy
-Tech City: Mountain View
-Tech State/Province: CA
-Tech Postal Code: 94043
-Tech Country: US
-Tech Phone: +1.6503300100
-Tech Phone Ext: 
-Tech Fax: +1.6506181499
-Tech Fax Ext: 
-Tech Email: dns-admin@google.com
-Name Server: ns3.google.com
-Name Server: ns2.google.com
-Name Server: ns1.google.com
-Name Server: ns4.google.com
-DNSSEC: unsigned
-URL of the ICANN WHOIS Data Problem Reporting System: http://wdprs.internic.net/
-";
+		$status = "$text,0";
+		file_put_contents($file, $status);
+		$msg = "Escribe el dominio a buscar";
 	break;
 
+	default:
+		//SI YA SE ESTÁ EJECUTANDO UN COMANDO
+		$st = explode(",", file_get_contents($file));
+
+		switch ($st[0]):
+
+			case "/firewall":
+				$ip = $text;
+				if (!filter_var($ip, FILTER_VALIDATE_IP) === false) :
+					$csf = shell_exec("/usr/bin/sudo /usr/sbin/csf -a $ip Bot consoluciones");
+					$hulk = shell_exec("/usr/bin/sudo /scripts/cphulkdwhitelist $ip");
+					$msg = "CSF: $csf\n";
+					$msg.= "CPHulk: $hulk";
+				else :
+					$msg = "$ip no es una IP válida";
+				endif;
+				$msg.= "\nPuede escribir otra IP";
+			break;
+
+			case "/ls":
+				$msg = shell_exec("/usr/bin/sudo ls -lh $text");
+				$msg.= "\nPuede escribir otra ruta a listar";
+			break;
+
+			case "/ping":
+				$msg = shell_exec("/usr/bin/sudo ping $text -c 4");
+				$msg.= "\nPuede escribir otro dominio o IP";
+			break;
+
+			case "/ps":
+				$msg = shell_exec("ps aux | grep $text");
+				$msg.= "\nPuede escribir otra palabra para filtrar";
+			break;
+
+			case "/whois":
+				$msg = shell_exec("/usr/bin/sudo whois $text");
+				$msg.= "\nPuede escribir otro dominio a buscar";
+			break;
+		endswitch;
+
+	break;
 endswitch;
 
-
-$sendto =API_URL."sendmessage?disable_web_page_preview=1&chat_id=".$chatID."&text=".urlencode($msg);
+$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".urlencode($msg);
 file_get_contents($sendto);
 ?>
